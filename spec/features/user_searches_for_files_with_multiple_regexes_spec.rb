890586aeac8f53a844a7b3c3cda8ab72b6dd14a9
Mikael Muszynski <mikael.muszynski@evokegaming.com>
2014-12-19 17:02:59 +0100

    Add spec_helper and pending feature spec

---
+++ spec/features/user_searches_for_files_with_multiple_regexes_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'Searching for files with multiple regexes' do
+  it 'returns filenames of the files where all regexes matched'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'filterfind'
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+end
