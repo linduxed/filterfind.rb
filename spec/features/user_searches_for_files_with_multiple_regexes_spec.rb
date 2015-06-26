5f5803dc37c47b3284c1d08b159fac667bda4780
Mikael Muszynski <mikael.muszynski@evokegaming.com>
2015-06-26 17:48:55 +0200

    Change procedure in feature spec and add context
    Since instances of Tempfile aren't placed in some dedicated directory
    (they're all placed in /tmp, just with unique names), modify the test so
    that the executable is provided with the paths of the tempfiles as
    arguments.

--- spec/features/user_searches_for_files_with_multiple_regexes_spec.rb
+++ spec/features/user_searches_for_files_with_multiple_regexes_spec.rb
@@ -2,25 +2,32 @@ require 'spec_helper'
 require 'tempfile'
 
 describe 'Searching for files with multiple regexes' do
-  it 'returns filenames of the files where all regexes matched' do
-    matching_file = Tempfile.new('matching')
-    matching_file.write("correct\n123\nxxVALIDxx\n")
-    matching_file.close
-    partially_matching_file = Tempfile.new('partially_matching')
-    partially_matching_file.write("wrong\nfoobar\ncorrect\n")
-    partially_matching_file.close
-    not_matching_file = Tempfile.new('not_matching')
-    not_matching_file.write("xxx\nyyy\nzzz\n")
-    not_matching_file.close
+  context 'file paths provided as arguments' do
+    it 'returns filenames of the files where all regexes matched' do
+      matching_file = Tempfile.new('matching')
+      matching_file.write("correct\n123\nxxVALIDxx\n")
+      matching_file.close
+      partially_matching_file = Tempfile.new('partially_matching')
+      partially_matching_file.write("wrong\nfoobar\ncorrect\n")
+      partially_matching_file.close
+      not_matching_file = Tempfile.new('not_matching')
+      not_matching_file.write("xxx\nyyy\nzzz\n")
+      not_matching_file.close
 
-    executable = Executable.run('-e "correct" -e "123" -i ".+valid.+"')
+      executable = Executable.run(
+        '-e "correct" -e "123" -i ".+valid.+" ' \
+        "#{matching_file.path} " \
+        "#{partially_matching_file.path} " \
+        "#{not_matching_file.path}"
+      )
 
-    expected_output_lines = [matching_file.path]
-    expect(executable.error).to be_empty, executable.error
-    expect(executable.lines).to eq(expected_output_lines)
+      expected_output_lines = [matching_file.path]
+      expect(executable.error).to be_empty, executable.error
+      expect(executable.lines).to eq(expected_output_lines)
 
-    [matching_file, partially_matching_file, not_matching_file].each do |file|
-      file.delete
+      [matching_file, partially_matching_file, not_matching_file].each do |file|
+        file.delete
+      end
     end
   end
 end
