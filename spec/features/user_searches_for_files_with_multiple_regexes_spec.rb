613da52b2bd77a16e944c3b7d9b5c1e92b0ae101
Mikael Muszynski <mikael.muszynski@evokegaming.com>
2015-10-25 18:13:19 +0100

    Expand test and remove pending status
    The current implementation already fulfills the requirements of the
    pending test description. Add the actual test to verify.
    
    As a part of expanding the test, write helper method for converting
    absolute paths into relative ones.

--- spec/features/user_searches_for_files_with_multiple_regexes_spec.rb
+++ spec/features/user_searches_for_files_with_multiple_regexes_spec.rb
@@ -59,7 +59,41 @@ describe 'Searching for files with multiple regexes' do
     end
 
     context 'files present in working directory' do
-      it 'returns filenames of the files where all regexes matched'
+      it 'returns filenames of the files where all regexes matched' do
+        Dir.mktmpdir do |wrapping_dir|
+          Dir.chdir(wrapping_dir) do
+            first_matching_file = Tempfile.new('matching', wrapping_dir)
+            first_matching_file.write("___\nfoobar\n___\n")
+            first_matching_file.close
+            second_matching_file = Tempfile.new('matching', wrapping_dir)
+            second_matching_file.write("___\n___\nfoobar\n")
+            second_matching_file.close
+            not_matching_file = Tempfile.new('not_matching', wrapping_dir)
+            not_matching_file.write("___\n___\n___\n")
+            not_matching_file.close
+
+            executable = Executable.run('-e "foobar"')
+
+            matching_files_as_relative_paths = [
+              first_matching_file.path,
+              second_matching_file.path
+            ].map do |path|
+              absolute_path_to_relative(base_dir: wrapping_dir, abs_path: path)
+            end
+            expect(executable.error).to be_empty, executable.error
+            expect(executable.lines).to include(
+              *matching_files_as_relative_paths
+            )
+            expect(executable.lines).not_to include(not_matching_file)
+
+            [
+              first_matching_file,
+              second_matching_file,
+              not_matching_file
+            ].each(&:delete)
+          end
+        end
+      end
 
       it 'returns filenames without leading dot dirs' do
         Dir.mktmpdir do |wrapping_dir|
@@ -78,6 +112,12 @@ describe 'Searching for files with multiple regexes' do
           end
         end
       end
+    end
+
+    def absolute_path_to_relative(base_dir:, abs_path:)
+      base = Pathname.new(base_dir).realpath
+
+      Pathname.new(abs_path).relative_path_from(base).to_s
     end
   end
 end
